56551b5bb8b9f2757d3cef8fe4515dc37dfebfcb
René Ruosch <rene.ruosch@amagoo.com>
2018-03-17 16:40:45 +0100

    Game Menu integriert

--- src/states/gamemenu.js
+++ src/states/gamemenu.js
@@ -1,72 +1,65 @@
-var GameMenu = function() {};
+/* globals __DEV__ */
+import Phaser from 'phaser'
+/* global game */
+var titleText;
+var startText;
+var optionCount;
 
-
-GameMenu.prototype = {
-/*
-  menuConfig: {
-    startY: 260,
-    startX: 30
-  },
-  */
-
-  init: function () {
+export default class extends Phaser.State {
+  init () {
+    this.keys = {}
     this.titleText = game.make.text(game.world.centerX, 100, "Lorem Ipsum", {
       fill: '#FDFFB5',
       align: 'center'
-    });
+    })
     this.titleText.anchor.set(0.5)
-    this.optionCount = 1
-    this.load.image('background', './assets/images/background.png')
-
+    this.startText = game.make.text(game.world.centerX, 150, "Start", {
+      fill: '#FDFFB5',
+      align: 'center'
+    })
+    this.startText.anchor.set(0.5)
+    this.optionCount = 1;
   }
 
-  create: function () {
-    game.stage.disableVisibilityChange = true;
-    game.add.sprite(0, 0, 'background');
-    game.add.existing(this.titleText);
+  preload () {
+    this.load.image('background', './assets/images/background.png')
+  }
 
-    // looks like we have to create a style for or menu option
-    var optionStyle = { fill: 'white', align: 'left' };
-    // the text for start
-    var txt = game.add.text(30, 280, 'Start', optionStyle);
-    // so how do we make it clickable?  We have to use .inputEnabled!
-    txt.inputEnabled = true;
-    // Now every time we click on it, it says "You did it!" in the console!
-    txt.events.onInputUp.add(function () { console.log('You did it!') });
+  create() {
+    game.stage.disableVisibilityChange = true
+    game.add.sprite(0, 0, 'background')
+    game.add.existing(this.titleText)
 
     this.addMenuOption('Start', function () {
-      game.state.start("Game");
+          game.state.start("Game");
     });
-    /*
-    this.addMenuOption('Options', function () {
-      game.state.start("Options");
-    });
-    this.addMenuOption('Credits', function () {
-      game.state.start("Credits");
-    });
-    */
+
   }
 
-  addMenuOption: function(text, callback) {
-     var optionStyle = { fill: 'white', align: 'left', stroke: 'rgba(0,0,0,0)', strokeThickness: 4};
-     var txt = game.add.text(30, (this.optionCount * 80) + 200, text, optionStyle);
-     var onOver = function (target) {
-       target.fill = "#FEFFD5";
-       target.stroke = "rgba(200,200,200,0.5)";
-     };
-     var onOut = function (target) {
-       target.fill = "white";
-       target.stroke = "rgba(0,0,0,0)";
-     };
-     txt.stroke = "rgba(0,0,0,0";
-     txt.strokeThickness = 4;
-     txt.inputEnabled = true;
-     txt.events.onInputUp.add(callback);
-     txt.events.onInputOver.add(onOver);
-     txt.events.onInputOut.add(onOut);
-     this.optionCount ++;
-   },
+  addMenuOption(text, callback) {
+   var txt = game.add.text(game.world.centerX, (this.optionCount * 80) + 200, text);
+   txt.anchor.setTo(0.5);
+   txt.stroke = "rgba(0,0,0,0";
+   txt.strokeThickness = 4;
 
-};
+   var onOver = function (target) {
+     target.fill = "#FEFFD5";
+     target.stroke = "rgba(200,200,200,0.5)";
+     txt.useHandCursor = true;
+   };
+   var onOut = function (target) {
+     target.fill = "white";
+     target.stroke = "rgba(0,0,0,0)";
+     txt.useHandCursor = false;
+   };
 
-// Phaser.Utils.mixinPrototype(GameMenu.prototype, mixins);
+   //txt.useHandCursor = true;
+   txt.inputEnabled = true;
+   txt.events.onInputUp.add(callback, this);
+   txt.events.onInputOver.add(onOver, this);
+   txt.events.onInputOut.add(onOut, this);
+
+   this.optionCount ++;
+ }
+
+}
