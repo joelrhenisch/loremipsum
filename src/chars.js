0ab2044fa99e3e44ca3f35ac1cb202bec70fc277
Ueli Kunz <kunz@ideadapt.net>
2018-03-17 17:46:27 +0100

    randomnezz

--- src/chars.js
+++ src/chars.js
@@ -1,1 +1,7 @@
-export default 'ejejej e'
+let text = ''
+const possible = 'abcdefghijklmnopqrstufwxyz'
+const length = 20
+
+for (let i = 0; i < length; i++) { text += possible.charAt(Math.floor(Math.random() * possible.length)) }
+
+export default text
