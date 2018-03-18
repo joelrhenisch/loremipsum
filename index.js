dcef92a80279cb33114816c6c17472f1a1124da1
Ueli Kunz <kunz@ideadapt.net>
2018-03-18 18:48:22 +0100

    fix path

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var static = require('node-static');
-var file = new static.Server();
+const static = require('node-static')
+const file = new static.Server('./build')
 require('http').createServer(function(request, response) {
   request.addListener('end', function() {
     file.serve(request, response);
