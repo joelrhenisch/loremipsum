1b79dcb1ba5369cfa073eed3ecff9a9e5d35d634
Ueli Kunz <kunz@ideadapt.net>
2018-03-18 17:47:52 +0100

    make deploy work

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -7,8 +7,10 @@ var CopyWebpackPlugin = require('copy-webpack-plugin')
 // Phaser webpack config
 var phaserModule = path.join(__dirname, '/node_modules/phaser-ce/')
 var phaser = path.join(phaserModule, 'build/custom/phaser-split.js')
+var phaserInputModule = path.join(__dirname, '/node_modules/@orange-games/phaser-input/')
 var pixi = path.join(phaserModule, 'build/custom/pixi.js')
 var p2 = path.join(phaserModule, 'build/custom/p2.js')
+var input = path.join(phaserInputModule, 'build/phaser-input.js')
 
 var definePlugin = new webpack.DefinePlugin({
   __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'false'))
@@ -66,6 +68,7 @@ module.exports = {
       { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') },
       { test: /pixi\.js/, use: ['expose-loader?PIXI'] },
       { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
+      { test: /phaser-input\.js$/, use: ['exports-loader?PhaserInput'] },
       { test: /p2\.js/, use: ['expose-loader?p2'] }
     ]
   },
@@ -78,7 +81,8 @@ module.exports = {
     alias: {
       'phaser': phaser,
       'pixi': pixi,
-      'p2': p2
+      'p2': p2,
+      'phaser-input': input
     }
   }
 }
